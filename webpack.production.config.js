a44f6e188b4de6b58eedb1082d96d34a64754b1d
Petar Bojinov <petarbojinov@gmail.com>
2016-03-17 16:26:30 -0700

    add style loader and css loader

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -5,43 +5,47 @@ var webpack = require('webpack');
 var del = require('del');
 
 class CleanPlugin {
-	constructor(options) {
-		this.options = options;
-	}
+  constructor(options) {
+    this.options = options;
+  }
 
-	apply () {
-		del.sync(this.options.files);
-	}
+  apply() {
+    del.sync(this.options.files);
+  }
 }
 
 module.exports = {
-	entry: './app/index',
-	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: 'app.min.js'
-	},
-	plugins: [
-		new webpack.optimize.OccurenceOrderPlugin(),
-		new CleanPlugin({
-			files: ['dist/*']
-		}),
-		new webpack.optimize.UglifyJsPlugin({
-			compressor: {
-				warnings: false,
-				screw_ie8: true
-			}
-		})
-	],
-	module: {
-		loaders: [{
-			test: /\.js?$/,
-			loader: 'babel',
-			include: path.join(__dirname, 'app'),
-			query: {
-				plugins: [
-					['transform-object-assign']
-				]
-			}
-		}]
-	}
+  entry: './app/index',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.min.js'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new CleanPlugin({
+      files: ['dist/*']
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+        screw_ie8: true
+      }
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.css$/,
+      loader: 'style!css?modules',
+      include: /flexboxgrid/
+    }, {
+      test: /\.js?$/,
+      loader: 'babel',
+      include: path.join(__dirname, 'app'),
+      query: {
+        plugins: [
+          ['transform-object-assign']
+        ]
+      }
+    }]
+  }
 };
