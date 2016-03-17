a3cc45943f630d0e255a0e524e18adb634c2935a
Petar Bojinov <petarbojinov@gmail.com>
2016-03-17 16:22:13 -0700

    use grid from example

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,39 +1,23 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import Helmet from 'react-helmet';
-import { Grid, Row, Col } from 'react-flexbox-grid';
+// import { Grid, Row, Col } from 'react-flexbox-grid';
+import { Grid } from 'react-flexbox-grid/lib/index';
 
 class App extends Component {
 
   render() {
     return (
       <div>
-				<Helmet
-      title='MyApp'
-      titleTemplate='MyApp - %s'
-      meta={[
-        {
-          'char-set': 'utf-8'
-        },
-        {
-          'name': 'description',
-          'content': 'My super dooper dope app'
-        }
-      ]}
-      />
-				<Grid>
-				<Row>
-				<nav>
-					<ul>
-						<li><Link to='/'>Users</Link></li>
-					</ul>
-				</nav>
-				</Row>
-				<Row>
-				{this.props.children}
-				</Row>
-				</Grid>
-			</div>
+		<Grid>
+			<nav>
+				<ul>
+					<li><Link to='/'>Users</Link></li>
+				</ul>
+			</nav>
+			{this.props.children}
+			</Grid>
+		</div>
       );
   }
 }
