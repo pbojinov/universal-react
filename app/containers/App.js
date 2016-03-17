d9f054615ef60acca351b4f43745fc3307e14307
Petar Bojinov <petarbojinov@gmail.com>
2016-03-17 16:20:01 -0700

    clean fork with react-flexbox-grid installed with basic webpack config

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,29 +1,41 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import Helmet from 'react-helmet';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 class App extends Component {
 
-	render() {
-		return (
-			<div>
+  render() {
+    return (
+      <div>
 				<Helmet
-					title='MyApp'
-					titleTemplate='MyApp - %s'
-					meta={[
-						{'char-set': 'utf-8'},
-						{'name': 'description', 'content': 'My super dooper dope app'}
-					]}
-				/>
+      title='MyApp'
+      titleTemplate='MyApp - %s'
+      meta={[
+        {
+          'char-set': 'utf-8'
+        },
+        {
+          'name': 'description',
+          'content': 'My super dooper dope app'
+        }
+      ]}
+      />
+				<Grid>
+				<Row>
 				<nav>
 					<ul>
 						<li><Link to='/'>Users</Link></li>
 					</ul>
 				</nav>
+				</Row>
+				<Row>
 				{this.props.children}
+				</Row>
+				</Grid>
 			</div>
-		);
-	}
+      );
+  }
 }
 
 export default App;
